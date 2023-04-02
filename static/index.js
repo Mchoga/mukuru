f325336d9e66388a929bfc55b52d7321226350dd
Mchoga <mchoga1036@gmail.com>
2023-04-02 20:16:26 +0200

    commit

---
+++ static/index.js
@@ -0,0 +1,20 @@
+console.log("Pressed");
+
+
+function showParagraph() {
+  var input = document.getElementsByClassName("wrong").value;
+  var paragraph = document.getElementById("wrong");
+//  alert("Please enter some text.");
+//  if (input !== "") {
+//    paragraph.style.display = "block";
+//  } else {
+//    alert("Please enter some text.");
+//  }
+console.log("Pressed");
+paragraph.style.display = "block";
+
+
+
+
+
+}
